6e03036eee7b219d0cdaceff40ad7ddac96a0950
Jake <newsomjk@gmail.com>
2026-05-05 22:10:10 -0400

    try to force a patch update

--- src/api/routes/assetPatch.routes.ts
+++ src/api/routes/assetPatch.routes.ts
@@ -79,6 +79,16 @@ const PATCHES: AssetPatch[] = [
     size: 0,
     description: "borders pack 1",
   },
+  {
+    id: "border-pack-1.5.1",
+    version: 5,
+    type: "graphics",
+    r2Key: "patches/borders-pack-1.5.1.zip",
+    checksum:
+      "sha256:cdd922140d11d2a7d4c3f0295c953c2fcd82577ab0628a4ec0d4a2f3871fdacc",
+    size: 0,
+    description: "border pack",
+  },
 ];
 
 function assetUrl(r2Key: string): string {
